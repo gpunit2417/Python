a903b5269ddddc998c6c598d649913507d655ef4
gpunit2417 <punitgoyal106@gmail.com>
2025-03-25 21:55:26 +0530

    string methods in python

---
+++ Practice/stringMethods.py
@@ -0,0 +1,20 @@
+str = "programming"
+str1 = "LANGUAGE"
+str2 = "     StringMethods       "
+# print(str.upper())
+# print(str2.lower())
+# print(str1.capitalize())
+# print(str.count("m"))
+# print(str1.lower().find("a"))
+# print(str1.find("A"))
+# print(str2.strip())
+# print(str2.replace("i", "o"))
+# print(str1.startswith("l"))
+# print(str1.startswith("L"))
+# print(str2.endswith("s"))
+# print(str2.endswith(" "))
+# print(len(str2))
+# print(str2[len(str2) - 1])
+
+print(str1.split())
+print(str1.encode("utf-8"))

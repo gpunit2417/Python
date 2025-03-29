a18476976352a409639e26a0a7a3d13e31a40e01
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 15:28:17 +0530

    access keys from dictionary

---
+++ Practice/dictionaryMethods.py
@@ -0,0 +1,7 @@
+dict1 = {"Name": "Punit", "Age": 20, "City": "Hisar"}
+
+# print(dict1["City"])
+# print(dict1.get("Name"))
+
+# print(dict1.get("rollno"))  #key absent, returns None 
+# print(dict1["rollno"])        #key absent, return KeyError

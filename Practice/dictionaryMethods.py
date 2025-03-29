e3f89c6459d20501d605d7d2e5af79f979419f30
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 15:31:40 +0530

    print all keys, values or items from the dictionary

--- Practice/dictionaryMethods.py
+++ Practice/dictionaryMethods.py
@@ -5,3 +5,12 @@ dict1 = {"Name": "Punit", "Age": 20, "City": "Hisar"}
 
 # print(dict1.get("rollno"))  #key absent, returns None 
 # print(dict1["rollno"])        #key absent, return KeyError
+
+print(dict1.keys())
+#output: dict_keys(['Name', 'Age', 'City'])
+
+print(dict1.values())
+#output: dict_values(['Punit', 20, 'Hisar'])
+
+print(dict1.items())
+#output: dict_items([('Name', 'Punit'), ('Age', 20), ('City', 'Hisar')])

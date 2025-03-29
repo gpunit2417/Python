581689c0656a33a2dd564f41904fe5e81e5b779c
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 11:49:48 +0530

    int basic methods

---
+++ Practice/intMethods.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the number:\n"))
+num1 = int(input("Enter second number:\n"))
+string = str(num)
+
+print(type(string))
+print(string)
+
+print(type(num))
+
+print(pow(num, num1))
+
+print(round(num))
+print(round(num1))
+print(round(3.9))

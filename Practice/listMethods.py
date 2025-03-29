fbf0e24452b672f47292a113c69eaa2ba725978e
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 11:58:27 +0530

    remove and count methods in list

--- Practice/listMethods.py
+++ Practice/listMethods.py
@@ -1,6 +1,12 @@
 list1 = [23, 34, 54, 56, 344, 53, 65]
-list1.append(12)
+# list1.append(12)
+# print(list1)
+
+# list1.pop()
+# print(list1)
+
+list1.remove(23)
 print(list1)
 
-list1.pop()
-print(list1)
+print(list1.count(23))
+print(list1.count(34))

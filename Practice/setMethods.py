1e3eee964c37d865581c77881a9c157042c14107
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 15:45:04 +0530

    remove methods of set

--- Practice/setMethods.py
+++ Practice/setMethods.py
@@ -1,12 +1,17 @@
 set1 = {1, 2, 3, 4, 5}
-set1.add(2)
-print(set1)
-#{1, 2, 3, 4, 5}
 
-set1.add(7)
-print(set1)
-#{1, 2, 3, 4, 5, 7}
+# set1.add(2)
+# print(set1)
+# #{1, 2, 3, 4, 5}
 
-set1.pop()
+# set1.add(7)
+# print(set1)
+# #{1, 2, 3, 4, 5, 7}
+
+# set1.pop()
+# print(set1)
+# #{2, 3, 4, 5, 7}
+
+set1.remove(5)
+# set1.remove(7)    #KeyError
 print(set1)
-#{2, 3, 4, 5, 7}

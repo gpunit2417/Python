b358bcad2a1d951f209c8b858b3a8e6e95c70f34
gpunit2417 <punitgoyal106@gmail.com>
2025-03-29 12:07:02 +0530

    insert, sort, reverse, and copy methods on the list

--- Practice/listMethods.py
+++ Practice/listMethods.py
@@ -5,8 +5,23 @@ list1 = [23, 34, 54, 56, 344, 53, 65]
 # list1.pop()
 # print(list1)
 
-list1.remove(23)
-print(list1)
+# list1.remove(23)
+# print(list1)
 
-print(list1.count(23))
-print(list1.count(34))
+# print(list1.count(23))
+# print(list1.count(34))
+
+# list1.insert(2, 23)
+# print(list1)
+
+# list1.sort()
+# print(list1)
+
+# list1.reverse()
+# print(list1)
+
+list2 = list.copy(list1)
+print(list2)
+
+list3 = list1
+print(list3)

0b2dc95338bd17228d54fbcca26991cb827f1a25
gpunit2417 <punitgoyal106@gmail.com>
2025-03-30 11:38:01 +0530

    alternative method for palindromic string

--- Practice/palindomicString.py
+++ Practice/palindomicString.py
@@ -1,8 +1,15 @@
+# str1 = "abba"
+
+# str2 = ''.join(reversed(str1))
+
+# if(str1 == str2):
+#     print("Palindromic")
+# else:
+#     print("Not Palindromic")
+
 str1 = "abba"
 
-str2 = ''.join(reversed(str1))
-
-if(str1 == str2):
+if str1 == str1[::-1]:  # Reverse the string using slicing
     print("Palindromic")
 else:
     print("Not Palindromic")

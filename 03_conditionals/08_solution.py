32b268ef17920a5a5520df5eeb224aafe65fd9db
gpunit2417 <punitgoyal106@gmail.com>
2025-04-04 11:00:02 +0530

    check password with uppercase, lowercase, numeral and special characters

--- 03_conditionals/08_solution.py
+++ 03_conditionals/08_solution.py
@@ -1,11 +1,32 @@
+# password = "Secure3P@ss"
+# password_length = len(password)
+
+# if len(password) < 6:
+#     strength = "Weak"
+# elif len(password) <= 10:
+#     strength = "Medium"
+# else:
+#     strength = "Strong"
+
+# print("Password strength is: ", strength)
+
+import string
+
 password = "Secure3P@ss"
 password_length = len(password)
 
-if len(password) < 6:
+# Criteria checks
+has_upper = any(char.isupper() for char in password)
+has_lower = any(char.islower() for char in password)
+has_digit = any(char.isdigit() for char in password)
+has_special = any(char in string.punctuation for char in password)
+
+# Determine strength
+if len(password) < 6 or not (has_upper and has_lower and has_digit and has_special):
     strength = "Weak"
 elif len(password) <= 10:
     strength = "Medium"
 else:
     strength = "Strong"
 
-print("Password strength is: ", strength)
+print("Password strength is:", strength)

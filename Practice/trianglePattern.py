487c418ca2076224ea7f9a034c57974b2f74cc24
gpunit2417 <punitgoyal106@gmail.com>
2025-04-08 21:41:23 +0530

    removed irrelevant question

--- Practice/trianglePattern.py
+++ Practice/trianglePattern.py
@@ -6,13 +6,3 @@
 
 # for i in range(1, 6): 
 #     print(" ".join(str(j) for j in range(1, i+1)))
-
-
-# mind blowing question
-def function(a, b=[]):
-    b.append(a)
-    return len(b)
-
-print(function(1))
-print(function(2, []))
-print(function(3))
